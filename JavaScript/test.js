543a1c1982504c2c91c6a6848d3505eb532ebb72
Your Name <your.email@example.com>
2025-10-31 15:00:54 +0100

    31.10.25 kl1500

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -9,5 +9,17 @@ console.log(myAge);
 
 console.log(`Hei jeg heter ${myName} og jeg er ${myAge} år gammel`);
 
+const tall = '18';
+const tekst = 'katter';
 
+console.log(tall + ' ' + tekst);
 
+console.log(myName + ' ' + myAge);
+
+console.log(24 * 60 * 60);
+
+let x =5;
+let y=10;
+console.log(x-y);
+
+console.log(23/5);
