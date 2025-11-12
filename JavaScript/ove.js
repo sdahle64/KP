f323b6f26d913e6e3758ddc660a7ec88692248b8
Your Name <your.email@example.com>
2025-11-12 11:21:59 +0100

    12.11.25 kl1121

--- JavaScript/ove.js
+++ JavaScript/ove.js
@@ -6,4 +6,18 @@ document.getElementById("mySubmit").onclick = function(){
     document.getElementById("myH1").textContent = `Hello ${username}`
   }
 
+/* If statement Under her */
+let age = 25;
+if(age <= 18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18+ to enter this site");
+}
+/* annet eksempel under her */
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+
 
