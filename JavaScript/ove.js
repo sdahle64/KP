d45cdfbd704b91dd23bc30c8b975f4f71358e12d
Your Name <your.email@example.com>
2025-11-12 17:21:27 +0100

    12.11.25 kl1721

--- JavaScript/ove.js
+++ JavaScript/ove.js
@@ -1,23 +1,42 @@
-/* JavaScript user input under her*/
-let username;
+// User input
+const usernameInput = document.getElementById("username");
+const submitName = document.getElementById("submitName");
+const usernameResult = document.getElementById("usernameResult");
+const heading = document.getElementById("myH1");
 
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`
-  }
+submitName.onclick = function() {
+    const username = usernameInput.value;
+    usernameResult.textContent = `Hello ${username}`;
+};
 
-/* If statement Under her */
-let age = 25;
-if(age <= 18){
-    console.log("You are old enough to enter this site");
+// if statements
+const ageInput = document.getElementById("age");
+const submitAge = document.getElementById("submitAge");
+const ageResult = document.getElementById("ageResult");
+
+submitAge.onclick = function() {
+    let age = Number(ageInput.value);
+
+    if (age >= 100) {
+        ageResult.textContent = `You are TOO OLD to enter this site`;
+    } 
+    else if (age === 0) {
+        ageResult.textContent = `You can´t enter. You were just born`;
+    } 
+    else if (age >= 18) {
+        ageResult.textContent = `You are old enough to enter this site`;
+    } 
+    else if (age > 0 && age < 18) {
+        ageResult.textContent = `You must be 18+ to enter this site`;
+    } 
+    else {
+        ageResult.textContent = `Invalid age`;
+    }
+};
+
+// ARRAYS
+let fruits = ["apple", "orange","banana", "coconut"];
+
+for(let i = 1; i < fruits.length; i+= 2){
+    console.log(fruits[i]);
 }
-else{
-    console.log("You must be 18+ to enter this site");
-}
-/* annet eksempel under her */
-
-const myText = document.getElementById("myText");
-const mySubmit = document.getElementById("mySubmit");
-const resultElement = document.getElementById("resultElement");
-
-
